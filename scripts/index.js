9a4e3aaaa6d853e83bf7c7240c7e01dd4ea11832
Koby <pkobybarrett@gmail.com>
2025-09-24 04:58:30 -0700

    commit for spots stage 5

--- scripts/index.js
+++ scripts/index.js
@@ -3,12 +3,31 @@ const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileModalCloseBtn = editProfileModal.querySelector(
   ".modal__close-button"
 );
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
+
 const newPostButton = document.querySelector(".profile__new-post-button");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostModalCloseBtn = newPostModal.querySelector(".modal__close-button");
+const newPostImageInput = editProfileModal.querySelector(
+  "#profile-image-link-input"
+);
+const newPostCaptionInput = editProfileModal.querySelector(
+  "#profile-caption-input"
+);
+
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
 
 editProfileButton.addEventListener("click", function () {
   editProfileModal.classList.add("modal_is-opened");
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
 });
 editProfileModalCloseBtn.addEventListener("click", function () {
   editProfileModal.classList.remove("modal_is-opened");
@@ -19,3 +38,20 @@ newPostButton.addEventListener("click", function () {
 newPostModalCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleEditProfileSubmit(event) {
+  event.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+  editProfileModal.classList.remove("modal_is-opened");
+}
+
+function handleNewPostSubmit(event) {
+  event.preventDefault();
+  console.log(newPostImageInput);
+  console.log(newPostCaptionInput);
+  editProfileModal.classList.remove("modal_is-opened");
+}
+
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
+editProfileForm.addEventListener("submit", handleNewPostSubmit);
